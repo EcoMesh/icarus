639a6f426ad6bd906fd9c0fab14ffb6b2db89084
NoahCardoza <noahcardoza@gmail.com>
2024-04-16 23:57:55 -0700

    query sensor locations from region event end

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI, Depends
-from pydantic import BaseModel
-from typing import List
+from pydantic import BaseModel, Field
+from typing import List, Literal, Annotated, Union
 from app.database import get_database
-
+import rethinkdb.query as r
 
 
 app = FastAPI()
@@ -12,15 +12,37 @@ class AlertSensor(BaseModel):
     node_id: str
     timestamp: str
 
-class Alert(BaseModel):
-    reason: str
-    state: str
+class AlertSensorStateChange(BaseModel):
+    reason: Literal["sensor_state_change"]
+    state: Literal["started", "ended"]
     alarm_id: str
-    region_id: str
     sensors: List[AlertSensor]
 
+
+class AlertRegionEvent(BaseModel):
+    reason: Literal["event_start", "event_end"]
+    alarm_id: str
+    alarm_event_id: str
+
+Alert = Annotated[Union[AlertSensorStateChange, AlertRegionEvent], Field(discriminator="reason")]
+
 @app.post("/webhook")
-def webhook(body: Alert, conn = Depends(get_database)):
-    print(body)
-    
-    return "Hello World!"
+async def webhook(body: Alert, conn = Depends(get_database)):
+    if body.reason == "event_end":
+        sensors = await (
+            r.table('alarms_event_records')
+            .get_all(body.alarm_event_id, index='alarm_event_id')
+            .eq_join("node_id", r.table("sensors"), index="node_id")
+            .run(conn)
+        )
+
+        coordinates = [
+            {
+                "latitude": sensor["right"]["location"]["coordinates"][0],
+                "longitude": sensor["right"]["location"]["coordinates"][1],
+            }
+            for sensor in sensors.items
+        ]
+        
+        return coordinates
+    return None
